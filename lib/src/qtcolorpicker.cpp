43eb8d90bc04f1753697b218417b5fc418a24231
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2018-12-30 04:58:58 +0100

    fix qt 5.12 compat

--- lib/src/qtcolorpicker.cpp
+++ lib/src/qtcolorpicker.cpp
@@ -896,12 +896,8 @@ void ColorPickerPopup::regenerateGrid()
 */
 void ColorPickerPopup::getColorFromDialog()
 {
-    bool ok;
-    QRgb rgb = QColorDialog::getRgba(lastSel.rgba(), &ok, parentWidget());
-    if (!ok)
-	return;
-
-    QColor col = QColor::fromRgba(rgb);
+    QColor col = QColorDialog::getColor(lastSel,parentWidget());
+    if (!col.isValid()) { return; }
     insertColor(col, tr("Custom"), -1);
     lastSel = col;
     emit selected(col);
