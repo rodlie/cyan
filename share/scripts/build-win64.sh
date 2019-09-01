1b8b7f4574c6fd23f54114faac1a50c41bd3155a
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-09-02 01:05:41 +0200

    win stuff

--- share/scripts/build-win64.sh
+++ share/scripts/build-win64.sh
@@ -14,31 +14,52 @@
 # Lesser General Public License for more details.
 #
 
+# Build Cyan for Windows x64 on Linux
+
+# Options:
+# MKJOBS : threads used to build
+# MAGICK : Magick++ pkg-config name
+# BUILD_DIR : Build output
+# MXE : MXE directory
+# MXE_TC : MXE toolchain
+
 CWD=`pwd`
-BUILD="$CWD/build-win64"
-MXE=/opt/Cyan-mxe
-TOOLCHAIN=x86_64-w64-mingw32.shared
-CMAKE=${TOOLCHAIN}-cmake
-STRIP=${TOOLCHAIN}-strip
-QT="$MXE/usr/$TOOLCHAIN/qt5"
-BIN="$MXE/usr/$TOOLCHAIN/bin"
-MAGICK=Magick++-7.Q16HDRI
-if [ -d "$CWD/mxe" ]; then
-    MXE="$CWD/mxe"
+MKJOBS=${MKJOBS:-2}
+MAGICK=${MAGICK:-Magick++-7.Q16HDRI}
+BUILD_DIR=${BUILD_DIR:-"${CWD}/build-win64"}
+MXE=${MXE:-/opt/Cyan-mxe}
+MXE_TC=${MXE_TC:-x86_64-w64-mingw32.shared}
+CMAKE=${MXE_TC}-cmake
+STRIP=${MXE_TC}-strip
+INNO_TAR="inno6.tar.xz"
+INNO_URL="https://sourceforge.net/projects/prepress/files/sdk/${INNO_TAR}/download"
+if [ -d "${CWD}/mxe" ]; then
+    MXE="${CWD}/mxe"
 fi
-INNO="$MXE/inno6/ISCC.exe"
+INNO="${MXE}/inno6/ISCC.exe"
 
-rm -rf "$BUILD" || true
-mkdir -p "$BUILD" || exit 1
-cd "$BUILD" || exit 1
+if [ ! -d "${MXE}" ]; then
+    echo "Please setup MXE!"
+    exit 1
+fi
 
-export PATH="$MXE/usr/bin:$PATH"
-export PKG_CONFIG_PATH="$MXE/usr/$TOOLCHAIN/lib/pkgconfig"
+if [ ! -f "${INNO}" ]; then
+    wget -O ${INNO_TAR} ${INNO_URL} || exit 1
+    tar xf ${INNO_TAR} -C "${MXE}/" || exit 1
+    rm -f ${INNO_TAR}
+fi
+
+rm -rf "${BUILD_DIR}" || true
+mkdir -p "${BUILD_DIR}" || exit 1
+cd "${BUILD_DIR}" || exit 1
+
+export PATH="${MXE}/usr/bin:${PATH}"
+export PKG_CONFIG_PATH="${MXE}/usr/${MXE_TC}/lib/pkgconfig"
 $CMAKE \
     -DCMAKE_BUILD_TYPE=Release \
-    -DMINGW_ROOT="$MXE/usr/$TOOLCHAIN" \
+    -DMINGW_ROOT="${MXE}/usr/${MXE_TC}" \
     -DCMAKE_INSTALL_PREFIX=/ \
-    -DMAGICK_PKG_CONFIG=$MAGICK "$CWD" || exit 1
-make -j2 || exit 1
-$STRIP -s *.exe *.dll */*.dll
-wine $INNO Cyan.iss
+    -DMAGICK_PKG_CONFIG=${MAGICK} "${CWD}" || exit 1
+make -j${MKJOBS} || exit 1
+${STRIP} -s *.exe *.dll */*.dll
+wine ${INNO} Cyan.iss
