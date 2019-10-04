d4d7c89ee3af03d1d5d067c3b5197dd7551ab7c2
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-10-05 00:50:27 +0200

    more cleanup

--- share/mingw/settings.mk
+++
@@ -1,6 +0,0 @@
-#JOBS := 4
-MXE_TARGETS := x86_64-w64-mingw32.shared
-MXE_PLUGIN_DIRS := plugins/gcc7
-LOCAL_PKG_LIST := cc cmake qtbase fontconfig lcms pango imagemagick7 ffmpeg
-.DEFAULT_GOAL := local-pkg-list
-local-pkg-list: $(LOCAL_PKG_LIST)
