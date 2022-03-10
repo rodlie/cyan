d3dd4b6f52cc507456a0a3404fffcfa7530f577b
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2022-03-10 18:07:13 +0100

    fix check for 32-bit support

--- src/cyan.cpp
+++ src/cyan.cpp
@@ -192,7 +192,7 @@ Cyan::Cyan(QWidget *parent)
     if (supportedDepth()>=16) {
         bitDepth->addItem(bitDepthIcon, tr("16-bit"), 16);
     }
-    if (supportedDepth()>=16 && fx.hasHDRI() ) {
+    if ( (supportedDepth() == 16 && fx.hasHDRI()) || supportedDepth() >= 32 ) {
         bitDepth->addItem(bitDepthIcon, tr("32-bit"), 32);
     }
     bitDepth->setMaximumWidth(150);
