e4c82a6224ef9eac2889af50b851f4c6a7b27fdf
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-02-02 17:48:40 +0100

    v3: handle read image failed

--- v3/cyan.cpp
+++ v3/cyan.cpp
@@ -798,9 +798,17 @@ Window::getTab(const QString &filename)
 void
 Window::handleOpenImageReady(const Engine::Image &image)
 {
-    if (isFileOpen(image.filename) ||
-        !image.success ||
-        image.buffer.length() < 1) { return; }
+    if ( isFileOpen(image.filename) ) { return; }
+
+    if (!image.success || image.buffer.length() <= 0 || image.width <= 0 || image.height <= 0) {
+        emit showStatusMessage(tr("Failed"), 500);
+        QFileInfo info(image.filename);
+        QMessageBox::warning( this,
+                              tr("Failed"),
+                              tr("Failed to read image %1.\n\n%2").arg(info.baseName(),
+                                                                       image.errors) );
+        return;
+    }
 
     auto cs = getColorSettings();
     cs.colorspace = image.colorspace;
